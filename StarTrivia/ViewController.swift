4e78c63343728cd1ed35bc10b47d9ffb51ac88d9
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2019-08-16 11:47:36 +0700

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Le Trung on 8/16/19.
+//  Copyright © 2019 Le Trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
